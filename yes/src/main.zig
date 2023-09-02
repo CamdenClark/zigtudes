7e8c3512717bb6b02284e5af3cf3dd1bac7d7987
hermiod <admin@hermiod.com>
2023-09-01 22:06:29 -0700

    yes functions appropriately

--- yes/src/main.zig
+++ yes/src/main.zig
@@ -1,1 +1,8 @@
-pub fn main() !void {}
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    while (true) {
+        _ = stdout.write("y\n") catch |e| return e;
+    }
+}
